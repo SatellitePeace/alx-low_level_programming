f82d418af535d70f5bf244ab0a8e47bf395c739b
SatellitePeace <njokunneoma1999@gmail.com>
2022-04-01 13:40:21 +0200

    Updated main.h file

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 
 int _putchar(char c);
@@ -23,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H */
